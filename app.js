c8561a1ec824678818e4d02f27d4001408f14062
Amit Hagag <amitadi91@gmail.com>
2022-11-01 20:08:06 +0700

    code rebuild and bug fixes

--- app.js
+++ app.js
@@ -1,67 +1,97 @@
 let numOfPlayers = 2;
-
-const numOfLottoNumbers = 6;
-const arrOfNumbers = [0, 0, 0, 0, 0, 0];
-const balls = {};
-
-const mainElement = document.querySelector("#main-element");
-
-//build the input of each user
+let numOfLottoNumbers = 6;
 const players = [];
 
-for (let user = 1; user <= numOfPlayers; user++) {
-  //object to follow data
-  const player = {
-    playerNumber: user,
-  };
-  player.balls = Array.from("0".repeat(numOfLottoNumbers)).map((el) => 0);
-  players.push(player);
+const body = document.querySelector("body");
+const settings = document.querySelector("#settings-wrapper");
+const lottoRoundsCounter = document.createElement("span");
 
-  //the base element
-  const userElement = document.createElement("div");
-  userElement.className = "user";
+const numOfPlayerSelector = document.querySelector("#num-of-playeres");
+numOfPlayerSelector.value = 2;
+numOfPlayerSelector.onchange = (e) => (numOfPlayers = e.target.value);
 
-  //the title
-  const titleForUser = document.createElement("span");
-  titleForUser.className = "user-selected-numbers-title";
-  titleForUser.innerText = `Player ${user} numbers:`;
+const numOfLottoNumbersSelector = document.querySelector("#num-of-balls");
+numOfLottoNumbersSelector.value = 6;
+numOfLottoNumbersSelector.onchange = (e) =>
+  (numOfLottoNumbers = e.target.value);
 
-  //the inputs
-  const inputsWrapper = document.createElement("div");
-  inputsWrapper.className = "inputs";
+function buildHtmlForLotto() {
+  body.removeChild(settings);
+  const lottoWrapper = document.createElement("div");
+  lottoWrapper.id = "lotto-wrapper";
+  //build the input of each user
+  for (let user = 1; user <= numOfPlayers; user++) {
+    //object to follow data
+    const player = {
+      playerNumber: user,
+    };
+    player.balls = Array.from("0".repeat(numOfLottoNumbers)).map((el) => 0);
+    players.push(player);
 
-  //button to genereate random number
-  const randNumButton = document.createElement("button");
-  randNumButton.innerText = "Random";
-  randNumButton.onclick = () => {
-    const numbers = generateLottoNumbers();
+    //the base element
+    const userElement = document.createElement("div");
+    userElement.className = "user";
 
-    for (let i = 0; i < numbers.length; i++) {
-      const input = document.querySelector(`#ball-${user}-${i}`);
-      input.value = numbers[i];
+    //the title
+    const titleForUser = document.createElement("span");
+    titleForUser.className = "user-selected-numbers-title";
+    titleForUser.innerText = `Player ${user} numbers:`;
 
-      players[user - 1].balls = numbers;
+    //the inputs
+    const inputsWrapper = document.createElement("div");
+    inputsWrapper.className = "inputs";
+
+    //button to genereate random number
+    const randNumButton = document.createElement("button");
+    randNumButton.innerText = "Random";
+    randNumButton.onclick = () => {
+      const numbers = generateLottoNumbers();
+
+      for (let i = 0; i < numbers.length; i++) {
+        const input = document.querySelector(`#ball-${user}-${i}`);
+        input.value = numbers[i];
+
+        players[user - 1].balls = numbers;
+      }
+    };
+
+    for (let i = 0; i < numOfLottoNumbers; i++) {
+      const input = document.createElement("input");
+      input.type = "number";
+      input.id = `ball-${user}-${i}`;
+      input.className = "number-input";
+      input.min = 0;
+      input.max = 37;
+      input.value = 0;
+      input.onchange = (e) => numChanged(user, e.target.value, i);
+
+      inputsWrapper.appendChild(input);
     }
+    //build the html
+    inputsWrapper.appendChild(randNumButton);
+    userElement.appendChild(titleForUser);
+    userElement.appendChild(inputsWrapper);
+    lottoWrapper.appendChild(userElement);
+    lottoWrapper.prepend(lottoRoundsCounter);
+  }
+
+  //button to start lotto
+  const buttonToStartLotto = document.createElement("button");
+  buttonToStartLotto.innerText = "START LOTTO";
+  buttonToStartLotto.onclick = () => lottoButtonClicked();
+
+  //button to restart lotto
+  const buttonToRestartLotto = document.createElement("button");
+  buttonToRestartLotto.innerText = "RESTART";
+  buttonToRestartLotto.onclick = () => {
+    body.removeChild(lottoWrapper);
+    body.appendChild(settings);
   };
 
-  for (let i = 0; i < numOfLottoNumbers; i++) {
-    const input = document.createElement("input");
-    input.type = "number";
-    input.id = `ball-${user}-${i}`;
-    input.className = "number-input";
-    input.min = 0;
-    input.max = 37;
-    input.value = 0;
-    input.onchange = (e) => numChanged(user, e.target.value, i);
+  lottoWrapper.appendChild(buttonToStartLotto);
+  lottoWrapper.appendChild(buttonToRestartLotto);
 
-    inputsWrapper.appendChild(input);
-  }
-  inputsWrapper.appendChild(randNumButton);
-
-  userElement.appendChild(titleForUser);
-  userElement.appendChild(inputsWrapper);
-
-  mainElement.appendChild(userElement);
+  body.appendChild(lottoWrapper);
 }
 
 const numChanged = (userNumber, num, idx) => {
@@ -71,33 +101,31 @@ const numChanged = (userNumber, num, idx) => {
   });
 };
 
-//button to start lotto
-const buttonToStartLotto = document.createElement("button");
-buttonToStartLotto.innerText = "START LOTTO";
-buttonToStartLotto.onclick = () => startLotto();
-
 function startLotto() {
   let counter = 0;
-  let winner = null;
 
   while (true) {
     const lottoNumbers = generateLottoNumbers();
-
     for (player of players) {
       const isEqual = player.balls.every((el) => lottoNumbers.includes(el));
       if (isEqual) {
-        winner = player;
-        break;
+        return { player, counter };
       }
     }
-
-    if (winner !== null) break;
-
     counter++;
   }
-  console.log(counter);
-  console.log(winner);
-  //something with the winner
+}
+
+function lottoButtonClicked() {
+  const result = startLotto();
+
+  for (let i = 0; i <= result.counter; i++) {
+    setTimeout(() => {
+      lottoRoundsCounter.innerHTML = i;
+    }, 2);
+  }
+
+  //do something with the winner
 }
 
 function generateLottoNumbers() {
