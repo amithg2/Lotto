b2eb7113d7c1a5acbc40663070e8b02ba5381561
Amit Hagag <amitadi91@gmail.com>
2022-11-01 15:11:43 +0700

    add run lotto function

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const numOfLottoNumbers = 6;
 const arrOfNumbers = [0, 0, 0, 0, 0, 0];
 const balls = {};
-const numbers = document.querySelector('#selected-numbers')
+const numbers = document.querySelector("#selected-numbers");
 
 const numChanged = (num, idx) => {
-  arrOfNumbers.splice(idx -1, 1, +num);
-  numbers.innerHTML= arrOfNumbers
+  arrOfNumbers.splice(idx - 1, 1, +num);
+  numbers.innerHTML = arrOfNumbers;
 };
 
 for (let i = 1; i <= numOfLottoNumbers; i++) {
@@ -15,4 +15,27 @@ for (let i = 1; i <= numOfLottoNumbers; i++) {
   );
 }
 
-console.log(balls);
+function generateLottoNumbers() {
+  //generate lotto numbers
+  const lottoNumbers = [];
+  while (lottoNumbers.length < numOfLottoNumbers) {
+    const num = Math.floor(Math.random() * 37 + 1);
+    if (!lottoNumbers.includes(num)) lottoNumbers.push(num);
+  }
+  return lottoNumbers;
+}
+
+function startLotto() {
+  let counter = 0;
+
+  while (true) {
+    const lottoNumbers = generateLottoNumbers();
+
+    const isEqual = lottoNumbers.every((el) => arrOfNumbers.includes(el));
+    if (isEqual) {
+      break;
+    }
+    counter++;
+  }
+  
+}
